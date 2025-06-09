0947f18b19babe0041e755ad7b675339282404bf
prince <princesuke@gmail.com>
2025-06-09 15:30:02 +0700

    feat: add search name

--- lab1/index.js
+++ lab1/index.js
@@ -10,16 +10,78 @@ const products = [
   { id: 2, name: "Keyboard", price: 500 },
 ];
 
+app.get("/products/search", (req, res) => {
+  const query = req.query.name;
+  if (!query) {
+    return res.status(400).json("Query parameter 'name' is required");
+  }
+
+  const result = products.filter((p) =>
+    p.name.toLowerCase().includes(query.toLowerCase())
+  );
+
+  res.json(result);
+});
+
 app.get("/products", (req, res) => {
   res.json(products);
 });
 
 app.get("/products/:id", (req, res) => {
   const id = Number(req.params.id);
+  const product = products.find((p) => p.id === id);
+  //   const index = products.findIndex((p) => p.id === id);
+  if (!product) return res.status(404).json({ error: "Product not found" });
+
+  res.json(product);
+});
+
+app.post("/products", (req, res) => {
+  const { name, price } = req.body;
+  const newProduct = {
+    id: products.length != 0 ? products[products.length - 1].id + 1 : 1,
+    name,
+    price,
+  };
+  products.push(newProduct);
+  res.status(201).json({
+    message: "Product addd",
+    product: newProduct,
+  });
+});
+
+app.put("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
   const index = products.findIndex((p) => p.id === id);
+
   if (index === -1) return res.status(404).json({ error: "Product not found" });
 
-  res.json(products[index]);
+  products[index] = { id, ...req.body };
+  res.json({ message: "Product Updated", product: products[index] });
+});
+
+app.patch("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const product = products.find((p) => p.id === id);
+  if (!product) return res.status(404).json({ error: "Product not found" });
+
+  Object.assign(product, req.body);
+
+  res.json({
+    message: "Product partially updated",
+    product,
+  });
+});
+
+app.delete("/products/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = products.findIndex((p) => p.id === id);
+
+  if (index === -1) return res.status(404).json({ error: "Product not found" });
+
+  products.splice(index, 1);
+
+  res.json({ message: "Product deleted" });
 });
 
 app.listen(PORT, () => {
